b726444d6e222b334988683088e74fe9e5a43bba
ShivaKumar <mail.shivaKumar03@gmail.com>
2023-04-21 13:06:24 +0530

    simple projects

---
+++ djpro/djProject/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjprojectConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "djProject"
--- simple.py
+++
@@ -1,5 +0,0 @@
-l = [1, 2, 3, 4]
-d = [1, 2, 3]
-l2 = l.extend(d)
-print(l2)
-print(l)
